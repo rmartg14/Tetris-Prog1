a176bc6399247ca4c6d63444b87a5c37a4f1eabd
Rubén Martínez <rmartg14@estudiantes.unileon.es>
2023-12-26 20:05:21 +0100

    Corrección en el drop

--- src/es/unileon/prg1/tetris/Board.java
+++ src/es/unileon/prg1/tetris/Board.java
@@ -115,7 +115,7 @@ public class Board {
         int x = block.getX();
         int y = block.getY();
         boolean canPlace=true;
-        if(x+block.getColumnsBlock()>=tablero[0].length){
+        if(x+block.getColumnsBlock()>=tablero[0].length+1){
             canPlace=false;
         }
         for (int i = 0; i < block.getRowsBlock(); i++) {
