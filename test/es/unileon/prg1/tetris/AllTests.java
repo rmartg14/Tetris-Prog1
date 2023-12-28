1b1b7f6fd92a34555986d8843142fa24156589c0
Rubén Martínez <rmartg14@estudiantes.unileon.es>
2023-12-28 16:34:40 +0100

    problema piececolor

--- test/es/unileon/prg1/tetris/AllTests.java
+++ test/es/unileon/prg1/tetris/AllTests.java
@@ -4,11 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import es.unileon.prg1.tetris.strategy.PieceColorTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ TetrisTest.class, ArrayMxNTest.class,ArrayNxNTest.class, BlockITest.class, BlockJTest.class, BlockLTest.class, BlockSTest.class, BlockTest.class,
-    BlockTTest.class, BlockZTest.class, BoardTest.class, PieceColorTest.class})
+    BlockTTest.class, BlockZTest.class, BoardTest.class})
 public class AllTests {
 
 }
